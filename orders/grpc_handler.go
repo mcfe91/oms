f90b32083d1e507b15faafd66d593d91efb3373a
Max McFerren <mmcferren91@gmail.com>
2024-05-10 17:26:59 -0400

    Clean up

--- orders/grpc_handler.go
+++ orders/grpc_handler.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"log"
 
-	common "github.com/mcfe91/commons"
 	pb "github.com/mcfe91/commons/api"
 	"github.com/mcfe91/commons/broker"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -50,39 +49,4 @@ func (h *grpcHandler) CreateOrder(ctx context.Context, p *pb.CreateOrderRequest)
 	})
 
 	return o, nil
-}
-
-// TODO: Something is wrong here...
-func (s *service) ValidateOrder(ctx context.Context, p *pb.CreateOrderRequest) error {
-	if len(p.Items) == 0 {
-		return common.ErrNoItems
-	}
-
-	mergedItems := mergeItemsQuantities(p.Items)
-	log.Print(mergedItems)
-
-	// validate with the stock service
-
-	return nil
-}
-
-func mergeItemsQuantities(items []*pb.ItemsWithQuantity) []*pb.ItemsWithQuantity {
-	merged := make([]*pb.ItemsWithQuantity, 0)
-
-	for _, item := range items {
-		found := false
-		for _, finalItem := range merged {
-			if finalItem.ID == item.ID {
-				finalItem.Quantity += item.Quantity
-				found = true
-				break
-			}
-		}
-
-		if !found {
-			merged = append(merged, item)
-		}
-	}
-
-	return merged
 }
