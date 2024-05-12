d1501bf3aa5fc6dc3b147be6bcff7768aab7e4cc
Max McFerren <mmcferren91@gmail.com>
2024-05-12 11:58:05 -0400

    Add stock data structure to store

--- stock/store.go
+++ stock/store.go
@@ -2,21 +2,51 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	pb "github.com/mcfe91/commons/api"
 )
 
 type store struct {
+	stock map[string]*pb.Item
 }
 
 func NewStore() *store {
-	return &store{}
+	return &store{
+		stock: map[string]*pb.Item{
+			"2": {
+				ID:       "2",
+				Name:     "Potato Chips",
+				PriceID:  "price_1PFel2CsyUNgRNLnfGo8VtJz",
+				Quantity: 10,
+			},
+			"1": {
+				ID:       "1",
+				Name:     "Cheese Burger",
+				PriceID:  "price_1PF100CsyUNgRNLnHgZ6933X",
+				Quantity: 20,
+			},
+		},
+	}
 }
 
 func (s *store) GetItem(ctx context.Context, id string) (*pb.Item, error) {
-	return nil, nil
+	for _, item := range s.stock {
+		if item.ID == id {
+			return item, nil
+		}
+	}
+
+	return nil, fmt.Errorf("item not found")
 }
 
 func (s *store) GetItems(ctx context.Context, ids []string) ([]*pb.Item, error) {
-	return nil, nil
+	var res []*pb.Item
+	for _, id := range ids {
+		if i, ok := s.stock[id]; ok {
+			res = append(res, i)
+		}
+	}
+
+	return res, nil
 }
