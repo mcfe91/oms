73b52c6c4eb23fc679c8db25ed721a80c28635d1
Max McFerren <mmcferren91@gmail.com>
2024-05-12 12:04:18 -0400

    Implement CheckIfItemsAreInStock

--- stock/service.go
+++ stock/service.go
@@ -14,10 +14,44 @@ func NewService(store StockStore) *service {
 	return &service{store}
 }
 
-func (s *service) CheckIfItemsAreInStock(ctx context.Context, items []*pb.ItemsWithQuantity) (bool, []*pb.Item, error) {
-	return false, nil, nil
+func (s *service) CheckIfItemsAreInStock(ctx context.Context, p []*pb.ItemsWithQuantity) (bool, []*pb.Item, error) {
+	itemIDs := make([]string, 0)
+	for _, item := range p {
+		itemIDs = append(itemIDs, item.ID)
+	}
+
+	itemsInStock, err := s.store.GetItems(ctx, itemIDs)
+	if err != nil {
+		return false, nil, err
+	}
+
+	// Check if all items are in stock
+	for _, stockItem := range itemsInStock {
+		for _, reqItem := range p {
+			if stockItem.ID == reqItem.ID && stockItem.Quantity < reqItem.Quantity {
+				return false, itemsInStock, nil
+			}
+		}
+	}
+
+	// create items with prices from stock
+	items := make([]*pb.Item, 0)
+	for _, stockItem := range itemsInStock {
+		for _, reqItem := range p {
+			if stockItem.ID == reqItem.ID {
+				items = append(items, &pb.Item{
+					ID:       stockItem.ID,
+					Name:     stockItem.Name,
+					PriceID:  stockItem.PriceID,
+					Quantity: reqItem.Quantity,
+				})
+			}
+		}
+	}
+
+	return true, items, nil
 }
 
 func (s *service) GetItems(ctx context.Context, ids []string) ([]*pb.Item, error) {
-	return nil, nil
+	return s.store.GetItems(ctx, ids)
 }
